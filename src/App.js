2823a00a0fe525908edf10aa7d0833a605c5e227
Rithik Dutt <duttrithik@gmail.com>
2022-01-08 13:14:01 +0530

    adding components
    Signed-off-by: Rithik Dutt <duttrithik@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Route} from 'react-router-dom';
+import Login from './Components/Login/Login.js';
+import Register from './Components/Register/Register.js';
+import Dashboard from './Components/Dashboard/Dashboard.js';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    //BEM conventions for naming react classes
+    <div className="app">
+      <Route exact path='/' component={Login}/>
+      <Route exact path='/register' component={Register}/>
+      <Route exact path='/in' component={Dashboard}/>
+    </div>
+  );
 }
 
 export default App;
