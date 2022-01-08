9e400a1ab431f0634218000bcd7211eab86adf70
Rithik Dutt <duttrithik@gmail.com>
2022-01-08 22:13:00 +0530

    Routes working
    Signed-off-by: Rithik Dutt <duttrithik@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Switch, Route} from 'react-router-dom';
+import {Routes, Route, BrowserRouter as Router} from 'react-router-dom';
 import Login from './Components/Login/Login.js';
 import Register from './Components/Register/Register.js';
 import Dashboard from './Components/Dashboard/Dashboard.js';
@@ -8,15 +8,13 @@ import Dashboard from './Components/Dashboard/Dashboard.js';
 function App() {
     return (
       //BEM conventions for naming react classes
-        <div className="app">
-          <Dashboard/>
-          <Login/>
-          <Register/>   
-          <Switch>
-            {/* <Route path="/login" component={Login}/>
-            <Route path="/register" component={Register}/> */}
-          </Switch>
-        </div>
+      <Router>
+        <Routes>
+          <Route exact path="/login" element={<Login/>}/>
+          <Route exact path="/register" element={<Register/>}/>
+          <Route exact path="/dashboard" element={<Dashboard/>}/>
+        </Routes>
+      </Router>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,8 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-  </React.StrictMode>,
+      <App/>,
   document.getElementById('root')
 );
